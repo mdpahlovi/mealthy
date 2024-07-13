c6f210188b21866c2d593e96f394e8a0a1639de2
MD Pahlovi <mdpahlovi07@gmail.com>
2024-07-13 18:21:57 +0600

    [Update] Auth Design

--- apps/client/src/utilities/supabase.ts
+++ apps/client/src/utilities/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://rbsmrtbmrcuvicrninoo.supabase.co";
+const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJic21ydGJtcmN1dmljcm5pbm9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA2MDIzOTcsImV4cCI6MjAzNjE3ODM5N30.EvRkh7VIswvanQyOT4n9vt_Z3L4MeDZCsNv0DfX4Bdw";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
